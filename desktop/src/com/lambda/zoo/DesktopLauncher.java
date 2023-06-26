47c9956b7625995c0b7778a28da74a9b0e6bf74f
Vivallo04 <andresvivallo4@gmail.com>
2023-06-25 23:36:47 -0600

    Initial LibGDX setup

---
+++ desktop/src/com/lambda/zoo/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.lambda.zoo;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.lambda.zoo.LambdaZooGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Lambda Zoo");
+		new Lwjgl3Application(new LambdaZooGame(), config);
+	}
+}
