f10604a70662a2f4efdf63a1af54ed79382a72be
Vivallo04 <andresvivallo4@gmail.com>
2023-06-30 13:28:45 -0600

    Configuration with map manager

--- core/src/com/lambda/zoo/LambdaZooGame.java
+++ core/src/com/lambda/zoo/LambdaZooGame.java
@@ -1,31 +1,18 @@
 package com.lambda.zoo;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import com.lambda.zoo.screens.MainGameScreen;
 
-public class LambdaZooGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class LambdaZooGame extends Game {
+	public static final MainGameScreen mainGameScreen = new MainGameScreen();
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(mainGameScreen);
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 1, 1, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		mainGameScreen.dispose();
 	}
 }
